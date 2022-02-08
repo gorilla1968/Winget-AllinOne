bc303debbd329df8f86acf1894d75b52f172df9f
Romain <96626929+Romanitho@users.noreply.github.com>
2022-02-08 16:46:52 +0100

    Add files via upload

--- install.bat
+++ install.bat
@@ -1,2 +1,2 @@
 @echo off
-powershell -Command "Get-ChildItem -Path '%~dp0' -Recurse | Unblock-File; Start-Process 'powershell.exe' -Wait -Argument '-executionpolicy bypass -file """%~dp0install.ps1"" '" -Verb RunAs
+powershell -Command "Get-ChildItem -Path '%~dp0' -Recurse | Unblock-File; Start-Process 'powershell.exe' -Wait -Argument '-executionpolicy bypass -file """%~dp0Winget-AllinOne.ps1"" '" -Verb RunAs
